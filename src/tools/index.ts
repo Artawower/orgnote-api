33652ac2af859c0d1d27d98a4ae5bf0c2568b1cd
Art <Artawower@mail.ru>
2024-09-16 03:19:30 +0400

    feat: export get file name as a tool func

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,5 +2,6 @@ export * from './mock-server';
 export * from './is-gpg-encrypted';
 export * from './is-org-file';
 export * from './get-string-path';
+export * from './get-file-name';
 export * from './find-notes-files-diff';
 export * from './join';
