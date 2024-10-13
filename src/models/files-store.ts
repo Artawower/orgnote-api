2def699824108d38b7afe070183aef8e778dfd1d
Art <Artawower@mail.ru>
2024-10-14 00:09:23 +0400

    feat: add support for save media file directly to files store

--- src/models/files-store.ts
+++ src/models/files-store.ts
@@ -3,6 +3,7 @@ import { Store } from './store';
 export interface FilesStore {
   getBlobUrl: (filePath: string) => Promise<string>;
   uploadMediaFile: (path?: string) => Promise<string>;
+  saveFile: (file: File, path?: string) => Promise<string>;
 }
 
 export type FilesStoreDefinition = Store<FilesStore>;
