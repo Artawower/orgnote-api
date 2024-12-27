f86667a01a4389e561a2c03327567778ac7bf595
Art <Artawower@mail.ru>
2024-12-27 17:40:57 +0530

    feat: new VueComponent type

--- src/models/vue-component.ts
+++ src/models/vue-component.ts
@@ -1,3 +1,8 @@
-export type VueComponent = {
-  __name: string;
-} & Record<string, unknown>;
+import type { DefineComponent } from 'vue';
+
+export type VueComponent<
+  Props = Record<string, unknown>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Emits = Record<string, any>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
+> = DefineComponent<Props, {}, any, {}, {}, {}, Emits>;
