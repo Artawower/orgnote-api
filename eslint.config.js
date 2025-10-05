d8f783fd5ba0ad89eb315489d4b83a29c50577ba
Art <Artawower@mail.ru>
2025-10-05 20:49:17 +0400

    feat: add sonarlintjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    {
+        plugins: { sonarjs },
+        rules: {
+            // TODO: feat/stable-beta enable before merge
+            'sonarjs/no-commented-code': 'off',
+        },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
 ];
