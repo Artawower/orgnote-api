6b67f6b4ec4806642d5f004dfa5c4b1d144a4621
Artur Iaroshenko <artawower@protonmail.com>
2026-04-04 19:35:14 +0400

    feat(commands): add navigation commands for editor

--- src/models/default-commands.ts
+++ src/models/default-commands.ts
@@ -177,6 +177,8 @@ export enum DefaultCommands {
   EDITOR_INSERT_TABLE = 'insert table',
   EDITOR_INSERT_TAG = 'insert tag',
   EDITOR_INSERT_DATETIME = 'insert datetime',
+  EDITOR_CARET_UP = 'caret up',
+  EDITOR_CARET_DOWN = 'caret down',
   EDITOR_HIDE_KEYBOARD = 'hide keyboard',
 
   // External commands
