a7680df0df0595b9a78228c5397b66e23e28f3a7
Artur Iaroshenko <artawower@protonmail.com>
2026-06-02 12:25:26 +0400

    refactor(org-edit): move createTask/CreateTaskInput to orgnote-api

--- src/utils/org-edit/index.ts
+++ src/utils/org-edit/index.ts
@@ -2,3 +2,5 @@ export * from './edit-org-document';
 export * from './types';
 export { createLogbook } from './features/logbook';
 export { createProperties } from './features/properties';
+export { createTask } from './features/create-task';
+export type { CreateTaskInput } from './features/create-task';
