d0a4cc6f72459fb3f95e672d601e3017b69e88fb
Artur Iaroshenko <artawower@protonmail.com>
2026-06-25 22:18:19 +0300

    feat(api): expose active document editing

--- src/utils/org-edit/__tests__/edit-org-document.spec.ts
+++ src/utils/org-edit/__tests__/edit-org-document.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { editOrgDocument } from '../edit-org-document';
+import { editOrgDocument, selectOrgDocument } from '../edit-org-document';
 
 test('editOrgDocument_returnsContentUnchanged_forNoOpMutate', () => {
   const content = '* TODO Task\nBody\n';
@@ -19,4 +19,12 @@ test('editOrgDocument_preservesRoundTrip_withPlanningLogbookPropertyDrawer', () 
   const content =
     '* TODO Daily\nSCHEDULED: <2026-05-13 Wed +1d>\n:LOGBOOK:\n- State "DONE" from "TODO" [2026-05-14 Thu 10:53]\n:END:\n:PROPERTIES:\n:STYLE: habit\n:END:\nBody\n';
   expect(editOrgDocument(content, () => {})).toBe(content);
+});
+
+test('selectOrgDocument_returnsSelectedValue_withoutMutation', () => {
+  const content = '* TODO Task\n:PROPERTIES:\n:Owner: Ada\n:END:\n';
+
+  const owner = selectOrgDocument(content, (doc) => doc.headlineAt(0)?.properties.get('Owner'));
+
+  expect(owner).toBe('Ada');
 });
