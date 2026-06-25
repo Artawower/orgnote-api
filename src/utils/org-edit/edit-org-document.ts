1143db388a2ee8f975f3b30d77f7f667bdf573eb
Artur Iaroshenko <artawower@protonmail.com>
2026-06-25 22:18:19 +0300

    feat(api): expose active document editing

--- src/utils/org-edit/edit-org-document.ts
+++ src/utils/org-edit/edit-org-document.ts
@@ -2,6 +2,14 @@ import { parse } from 'org-mode-ast';
 import { createOrgDocument } from './document';
 import type { OrgDocument } from './types';
 
+export const selectOrgDocument = <T>(
+  content: string,
+  select: (doc: OrgDocument) => T
+): T => {
+  const root = parse(content);
+  return select(createOrgDocument(root));
+};
+
 export const editOrgDocument = (
   content: string,
   mutate: (doc: OrgDocument) => void
