53a887b857fd5ff3c85d449c1addf0d658e78902
Artur Iaroshenko <artawower@protonmail.com>
2026-06-25 22:18:19 +0300

    feat(api): expose active document editing

--- src/models/editor-store.ts
+++ src/models/editor-store.ts
@@ -1,4 +1,6 @@
 import type { ComputedRef, ShallowRef } from 'vue';
+import type { EditorView } from '@codemirror/view';
+import type { OrgNode } from 'org-mode-ast';
 import type {
   InlineEmbeddedWidgets,
   MultilineEmbeddedWidgets,
@@ -8,6 +10,18 @@ import type { EditorExtension } from './editor';
 import type { WidgetMeta } from '../api';
 import type { StoreDefinition } from './store';
 import type { ActiveEditorContext } from './active-editor-context';
+import type { OrgDocument } from '../utils/org-edit/types';
+
+export interface ActiveDocumentEditContext extends ActiveEditorContext {
+  readonly doc: OrgDocument;
+  readonly root: OrgNode;
+  readonly view: EditorView;
+}
+
+export interface ActiveDocumentEditOptions {
+  readonly preserveSelection?: boolean;
+  readonly scrollIntoView?: boolean;
+}
 
 export interface EditorStore {
   inlineWidgets: ComputedRef<InlineEmbeddedWidgets>;
@@ -25,6 +39,10 @@ export interface EditorStore {
   selection: ComputedRef<string>;
   hasSelection: ComputedRef<boolean>;
 
+  editActiveDocument: (
+    mutate: (ctx: ActiveDocumentEditContext) => void,
+    options?: ActiveDocumentEditOptions
+  ) => boolean;
   setActiveContext: (ctx: Partial<ActiveEditorContext>) => void;
   updateActiveContext: (ctx: Partial<ActiveEditorContext>) => void;
   clearActiveContext: () => void;
