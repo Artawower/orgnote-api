218b16321e8e3406e3751cd528bdb6654b481ec9
Artur Iaroshenko <artawower@protonmail.com>
2026-08-01 20:45:43 +0300

    feat(viewers): add view state contract

--- src/models/buffer-viewer-store.ts
+++ src/models/buffer-viewer-store.ts
@@ -3,11 +3,32 @@ import type { StoreDefinition } from './store';
 
 export type BufferViewerComponent = Component | (() => Promise<Component>);
 
+export type SerializableViewState =
+  | null
+  | boolean
+  | number
+  | string
+  | readonly SerializableViewState[]
+  | { readonly [key: string]: SerializableViewState };
+
+export interface BufferViewStateHandle<
+  TState extends SerializableViewState = SerializableViewState,
+> {
+  get: () => TState | undefined;
+  set: (state: TState) => void;
+  clear: () => void;
+}
+
+export interface ViewerViewStateConfig {
+  version: number;
+}
+
 export interface ViewerMeta {
   id: string;
   name: string;
   icon?: string;
   priority?: number;
+  viewState?: ViewerViewStateConfig;
 }
 
 export interface BufferViewerEntry {
